9052672b6a433ce13f686bd9faecc30f4ea2e2e6
zen <zhaozeen1996@163.com>
2017-06-20 19:42:54 +0800

    daily exercises

---
+++ ReversString2.java
@@ -0,0 +1,28 @@
+package com.ze.offer;
+
+/**
+ * 
+ * @author ZEN 题目：将一段字符串中的每2k个字符中的前k个字符反转
+ */
+public class ReversString2 {
+
+	public String reverseStr(String s, int k) {
+		char[] arr = s.toCharArray();
+		int n = arr.length;
+		int i = 0;
+		while (i < n) {
+			int j = Math.min(i + k - 1, n - 1);
+			swap(arr, i, j);
+			i += 2 * k;
+		}
+		return String.valueOf(arr);
+	}
+
+	private void swap(char[] arr, int l, int r) {
+		while (l < r) {
+			char temp = arr[l];
+			arr[l++] = arr[r];
+			arr[r--] = temp;
+		}
+	}
+}
