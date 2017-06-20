853f118d29f895c29472e9ecdc7611a708243cc7
zen <zhaozeen1996@163.com>
2017-06-20 16:34:46 +0800

    daily exercises

--- RandomizedCollection.java
+++ RandomizedCollection.java
@@ -7,6 +7,14 @@ import java.util.Set;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author ZEN
+ *题目：完成一个集合，使其在O(1)时间内完成插入，删除，并可以返回其中的一个随机元素。
+ *思路:借助hashmap，HashMap<Integer,LinkedHashSet<Interger>>，其中LinkedHashSet中储存数字的存放下标。
+ *插入：若未包含，则在hashmap中存放当前数字，并新建下标列表；若已经存在，则在hashmap中添加数字对应的下标至下标列表中。
+ *删除：判断是否已被包含，若未，则直接返回false。若被包含，获取该数字的下标，并在hashmap中将该下标删除。将最后一个数字改到被删除元素的位置，并更新hashmap。最后将ArrayList中的最后一个元素删除即可。这样，只需要O(1)的时间就可以完成删除
+ */
 public class RandomizedCollection {
 	ArrayList<Integer> nums;
 	HashMap<Integer, Set<Integer>> locs;
@@ -58,10 +66,10 @@ public class RandomizedCollection {
 	public int getRandom() {
 		return nums.get(rand.nextInt(nums.size()));
 	}
-	
+
 	@Test
-	public void test(){
-		RandomizedCollection rc=new RandomizedCollection();
+	public void test() {
+		RandomizedCollection rc = new RandomizedCollection();
 		rc.insert(1);
 		rc.insert(1);
 		rc.insert(15);
@@ -69,6 +77,6 @@ public class RandomizedCollection {
 		rc.remove(1);
 		rc.insert(2);
 		rc.remove(3);
-		
+
 	}
 }
