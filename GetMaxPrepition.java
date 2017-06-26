f00d8a099b124161b35d3b05850a6678927a3615
zen <zhaozeen1996@163.com>
2017-06-26 19:59:25 +0800

    daily exercises

--- GetMaxPrepition.java
+++ GetMaxPrepition.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 
 public class GetMaxPrepition {
 	/**
-	 *Q: Define S = [s,n] as the string S which consists of n connected strings s.
-	 * For example, ["abc", 3] ="abcabcabc".
+	 * Q: Define S = [s,n] as the string S which consists of n connected strings
+	 * s. For example, ["abc", 3] ="abcabcabc".
 	 * 
 	 * On the other hand, we define that string s1 can be obtained from string
 	 * s2 if we can remove some characters from s2 such that it becomes s1. For
@@ -16,6 +16,12 @@ public class GetMaxPrepition {
 	 * characters long) and two integers 0 ≤ n1 ≤ 106 and 1 ≤ n2 ≤ 106. Now
 	 * consider the strings S1 and S2, where S1=[s1,n1] and S2=[s2,n2]. Find the
 	 * maximum integer M such that [S2,M] can be obtained from S1.
+	 * 
+	 * A:1.How do we know "string s2 can be obtained from string s1"? 
+	 * Easy, use two pointers iterate through s2 and s1. If chars are equal, move both.
+	 * Otherwise only move pointer1. 
+	 * 2.We repeat step 1 and go through s1 for n1 times and count how many times can we go through s2. 
+	 * 3.Answer to this problem is times go through s2 divide by n2.
 	 * 
 	 * @param s1
 	 * @param n1
@@ -29,18 +35,18 @@ public class GetMaxPrepition {
 		char[] array1 = s1.toCharArray();
 		char[] array2 = s2.toCharArray();
 		int count1 = 0, count2 = 0, i = 0, j = 0;
-		while (count1 < n1) {
+		while (count1 < n1) {// 当累计次数小于n1
 			if (array2[i] == array1[j]) {
 				i++;
-				count2++;
 			}
 			j++;
-			if (j == array1.length) {
+			if (j == array1.length) {// 当已经循环过一次s1
 				count1++;
 				j = 0;
 			}
-			if (i == array2.length) {
+			if (i == array2.length) {// 已经循环过一次s2
 				i = 0;
+				count2++;
 			}
 		}
 
