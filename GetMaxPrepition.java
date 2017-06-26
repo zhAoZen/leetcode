603f793c6d7a53dc8433b1065b3c8cc79b830e34
zen <zhaozeen1996@163.com>
2017-06-26 19:51:31 +0800

    daily exercises

---
+++ GetMaxPrepition.java
@@ -0,0 +1,56 @@
+package com.ze.offer;
+
+import org.junit.Test;
+
+public class GetMaxPrepition {
+	/**
+	 *Q: Define S = [s,n] as the string S which consists of n connected strings s.
+	 * For example, ["abc", 3] ="abcabcabc".
+	 * 
+	 * On the other hand, we define that string s1 can be obtained from string
+	 * s2 if we can remove some characters from s2 such that it becomes s1. For
+	 * example, “abc” can be obtained from “abdbec” based on our definition, but
+	 * it can not be obtained from “acbbe”.
+	 * 
+	 * You are given two non-empty strings s1 and s2 (each at most 100
+	 * characters long) and two integers 0 ≤ n1 ≤ 106 and 1 ≤ n2 ≤ 106. Now
+	 * consider the strings S1 and S2, where S1=[s1,n1] and S2=[s2,n2]. Find the
+	 * maximum integer M such that [S2,M] can be obtained from S1.
+	 * 
+	 * @param s1
+	 * @param n1
+	 * @param s2
+	 * @param n2
+	 * @return
+	 */
+	public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+		// abab acbacbacbacb
+		// acb 4 ab 2
+		char[] array1 = s1.toCharArray();
+		char[] array2 = s2.toCharArray();
+		int count1 = 0, count2 = 0, i = 0, j = 0;
+		while (count1 < n1) {
+			if (array2[i] == array1[j]) {
+				i++;
+				count2++;
+			}
+			j++;
+			if (j == array1.length) {
+				count1++;
+				j = 0;
+			}
+			if (i == array2.length) {
+				i = 0;
+			}
+		}
+
+		return count2 / n2;
+	}
+
+	@Test
+	public void Test() {
+		String s1 = "acb";
+		String s2 = "ab";
+		System.out.println(getMaxRepetitions(s1, 4, s2, 2));
+	}
+}
--- ReversString2.java
+++ ReversString2.java
@@ -25,4 +25,4 @@ public class ReversString2 {
 			arr[r--] = temp;
 		}
 	}
-}
+}		
